cbb99c4cd0fce02766718ddd9a1357179d40d862
Ben Glasser <ben@benglasser.com>
2025-05-17 02:36:34 -0600

    edit nav

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,22 +1,49 @@
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
+import siteMetadata, { author, email, github, linkedin, twitter } from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import Image from 'next/image'
+import { coreContent } from 'pliny/utils/contentlayer'
+import { allAuthors, Authors } from 'contentlayer/generated'
+import SocialIcon from '@/components/social-icons'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
 
 const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
+  const author = allAuthors.find((p) => p.slug === 'default') as Authors
+  const { avatar, name, occupation, company, email, twitter, bluesky, linkedin, github } = author
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Latest
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
+          <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+            <div className="flex flex-col items-center space-x-2 pt-8">
+              {avatar && (
+                <Image
+                  src={avatar}
+                  alt="avatar"
+                  width={192}
+                  height={192}
+                  className="h-48 w-48 rounded-full"
+                />
+              )}
+              <h3 className="pt-4 pb-2 text-2xl leading-8 font-bold tracking-tight">{name}</h3>
+              <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
+              <div className="text-gray-500 dark:text-gray-400">{company}</div>
+              <div className="flex space-x-3 pt-6">
+                <SocialIcon kind="mail" href={`mailto:${email}`} />
+                <SocialIcon kind="github" href={github} />
+                <SocialIcon kind="linkedin" href={linkedin} />
+                <SocialIcon kind="x" href={twitter} />
+                <SocialIcon kind="bluesky" href={bluesky} />
+              </div>
+            </div>
+            <div className="prose dark:prose-invert max-w-none pt-8 pb-8 xl:col-span-2">
+              <MDXLayoutRenderer code={author.body.code} />
+            </div>
+          </div>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
