6ea1df4341998a32c5bcf1bb9888c6c1bd676d88
Ben Glasser <ben@benglasser.com>
2025-05-17 00:09:19 -0600

    clean up

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -13,14 +13,6 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/blog/starfield.gif',
     href: '/blog/20250516-StarfieldVisualizer',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
 ]
 
 export default projectsData
