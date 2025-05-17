905d7912c7360036d43d58b68572207f7e4092b5
Ben Glasser <ben@benglasser.com>
2025-05-17 01:10:26 -0600

    auto load comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import { Comments as CommentsComponent } from 'pliny/comments'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
   const [loadComments, setLoadComments] = useState(false)
+
+  useEffect(() => {
+    setLoadComments(true)
+  }, [])
 
   if (!siteMetadata.comments?.provider) {
     return null
