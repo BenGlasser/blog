f61ebf7fe5a84297c4643d40d21e803b47ad9567
Ben Glasser <ben@benglasser.com>
2025-08-24 11:49:22 -0600

    Adds 'Resume' link to navigation
    Inserts a 'Resume' link in header navigation for easier access.
    Removes duplicate image file due to incorrect file handling.

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/blog/resume', title: 'Resume' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/tags', title: 'Tags' },
